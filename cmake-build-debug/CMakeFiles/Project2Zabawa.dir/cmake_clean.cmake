6bdd5e3dff1953c033986a755e6cea53608fb13e
puscik21 <grzesiekpuscizna@gmail.com>
2020-12-27 21:06:24 +0100

    Init board preparetion

---
+++ cmake-build-debug/CMakeFiles/Project2Zabawa.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project2Zabawa.dir/main.cpp.obj"
+  "Project2Zabawa.exe"
+  "Project2Zabawa.exe.manifest"
+  "Project2Zabawa.pdb"
+  "libProject2Zabawa.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project2Zabawa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
